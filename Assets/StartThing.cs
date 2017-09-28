5952de571ea25ccfc1f8ad9def3e8d807b810f72
Unknown <cef363@nyu.edu>
2017-09-27 17:59:30 -1000

    1st NEW Rube Goldberg Commit

---
+++ Assets/StartThing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//put this on the trapdoor
+public class StartThing : MonoBehaviour {
+
+	public Rigidbody trapDoor;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			trapDoor.constraints = RigidbodyConstraints.None;
+		}
+	}
+}
